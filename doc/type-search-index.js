bf6f048342b3ca5ecfc41857ad8e8671e3447d0c
hugoarivera <147124618+hugoarivera@users.noreply.github.com>
2023-11-09 21:48:51 -0600

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop10","l":"CuentaBancaria"},{"p":"poop10","l":"POOP10"},{"p":"poop10","l":"SaldoInsuficienteException"}];updateSearchResults();
